1dbdbf6037fa22e00bb96eb5097ecbf34717e96a
zhupan <zhupan97njy@gmail.com>
2021-12-28 19:19:19 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -23,11 +23,8 @@
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
             <div class="card ">
                 <div class="card-body">
-                    <h5><strong>个人简介</strong></h5>
-                    <p>{{ $user->introduction }}</p>
-                    <hr>
-                    <h5><strong>注册于</strong></h5>
-                    <p>{{ $user->created_at->diffForHumans() }}</p>
+                    <h1 class="mb-0" style="font-size:22px;">{{ $user->name }}
+                        <small>{{ $user->email }}</small></h1>
                 </div>
             </div>
             <hr>
