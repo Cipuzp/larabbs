94c4d9072252147132eaf99e11da2a40f3b806cb
zhupan <zhupan97njy@gmail.com>
2021-12-28 14:56:31 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="https://weibo.com/3160054022/profile?topnav=1&wvr=6&is_all=1" target="_blank">朱攀</a> 设计和编码 <span
+                style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:zhupan97njy@163.com">联系我们</a></p>
+    </div>
+</footer>
